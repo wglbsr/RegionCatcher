a3365864b1afbe105cbcb18d058673f784420767
lane <wglbsr@163.com>
2019-02-19 17:32:12 +0800

    update almost done

--- src/main/java/com/wgl/tool/DataCatcher.java
+++ src/main/java/com/wgl/tool/DataCatcher.java
@@ -1,5 +1,6 @@
 package com.wgl.tool;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -7,8 +8,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,7 +20,9 @@ import java.util.regex.Pattern;
 public class DataCatcher {
     private static final String BASE_URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/";
     private static final String START_URL = "index.html";
-    private static String REGION_ID_REG_EXP = "\\d{2,12}(<=.html)";//正则表达式,后发零宽断言
+    private static String REGION_ID_REG_EXP = "(<=/)\\d{2,12}(<=.html)";//正则表达式,后发和先行零宽断言
+    private static final int MAX_LEVEL = 5;
+    private static String[] ELE_LIST = new String[MAX_LEVEL];
 
     public static void main(String[] args) {
         DataCatcher dataCatcher = new DataCatcher();
@@ -29,8 +30,13 @@ public class DataCatcher {
     }
 
     public void start() {
-        JSONObject jsonObject = new JSONObject();
-        Document document = getHtmlContent(START_URL);
+        ELE_LIST[0] = "tr.provincetr td a";
+        ELE_LIST[1] = "tr.citytr td a";
+        ELE_LIST[2] = "tr.counrytr td a";
+        ELE_LIST[3] = "tr.towntr td a";
+        ELE_LIST[4] = "tr.villagetr td";
+
+        getProvincesElement(START_URL);
     }
 
 
@@ -43,6 +49,7 @@ public class DataCatcher {
         }
         return document;
     }
+    //provincetr,citytr,countytr,towntr,villagetr
 
     /**
      * @return org.jsoup.select.Elements
@@ -51,24 +58,36 @@ public class DataCatcher {
      * @Date 16:23 2019-02-19
      * @Param [document]
      **/
-    private Elements getProvincesElement(Document document) {
-        Elements provinceLines = document.select("provincetr");
-        Elements elements = new Elements();
-        for (Element provinceLine : provinceLines) {
-            Elements provinces = provinceLine.select("td.a");
-            elements.addAll(provinces);
-        }
-        return elements;
+    private JSONObject getProvincesElement(String url) {
+        JSONObject jsonObject = new JSONObject();
+        Document document = getHtmlContent(url);
+        Elements provinceLines = document.select("tr.provincetr td a");
+
+        return jsonObject;
     }
 
+    private static final String KEY_ID = "id";
+    private static final String KEY_PARENT_ID = "parent_id";
+    private static final String KEY_LEVEL = "level";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_CHILDREN = "children";
 
-    private List<String> getUrls(Elements elements) {
-        List<String> urls = new ArrayList<String>();
+    private JSONArray get(String url, int level, JSONObject regionJsonObj) {
+        Document document = getHtmlContent(url);
+        String parentId = findStrByRegEx(url, REGION_ID_REG_EXP);
+        Elements elements = document.select(ELE_LIST[level]);
         for (Element element : elements) {
-            String url = element.attr("href");
-            String regionId = findStrByRegEx(url, REGION_ID_REG_EXP);
+            JSONObject jsonObject = new JSONObject();
+            String name = element.text();
+            jsonObject.put(KEY_PARENT_ID, parentId);
+            jsonObject.put(KEY_NAME, name);
+            jsonObject.put(KEY_LEVEL, level);
+
+            String nextUrl = element.attr("href");
         }
-        return urls;
+
+
+        return null;
     }
 
 
